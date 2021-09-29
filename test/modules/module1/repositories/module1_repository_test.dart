df28847268e0d5c25f27bcff659c560c912dc9be
Ali <dev.aliakbar@gmail.com>
2021-09-29 15:35:21 +0530

    Added repo in repository test

--- test/modules/module1/repositories/module1_repository_test.dart
+++ test/modules/module1/repositories/module1_repository_test.dart
@@ -2,6 +2,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 import 'package:project_structure_xcelpros/core/local_storage.dart';
 import 'package:project_structure_xcelpros/core/network_utils.dart';
+import 'package:project_structure_xcelpros/modules/module1/repositories/module1_repository.dart';
 
 class MockLocalStorage extends Mock implements LocalStorage {}
 
@@ -10,6 +11,9 @@ class MockNetworkUtils extends Mock implements NetworkUtils {}
 void main() {
   final MockLocalStorage mockLocalStorage = MockLocalStorage();
   final MockNetworkUtils mockNetworkUtils = MockNetworkUtils();
+
+  final Module1Repository module1repository = Module1Repository(
+      localStorage: mockLocalStorage, networkUtils: mockNetworkUtils);
 
   final Map<String, dynamic> testLocalResponse = {"name": "LOCAL DATA LOADED"};
 
